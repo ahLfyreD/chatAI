c3108e65cfc3c517a840b0fdd5f4193ee60cb9e2
Alfred <fredileader@gmail.com>
2025-03-06 02:20:14 +0100

    fixed endpoint error

--- frontend/app/components/ChatBot.tsx
+++ frontend/app/components/ChatBot.tsx
@@ -21,7 +21,7 @@ const ChatBot: React.FC<ChatBotProps> = ({ toggleView }) => {
     setLoading(true);
 
     try {
-      const response = await axios.post("http://45.45.163.144/chat", {
+      const response = await axios.post("https://45.45.163.144/chat", {
         message: userMessage,
       }, {
         headers: {
